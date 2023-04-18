071b2396d9270e945eec306ae2415ba0301cc8e5
Arulkumar, Anu <anu.arulkumar@accenture.com>
2023-04-18 09:42:59 -0400

    Weather card with test data from props

--- src/CityWeather.js
+++ src/CityWeather.js
@@ -1,12 +1,10 @@
 import WeatherCard from "./WeatherCard";
 import './CityWeather.css';
 
-function CityWeather(){
+function CityWeather(props){
     return (
         <div className="WeatherCards">
-          <WeatherCard city="Toronto" current="8" high="11" low="2" humidity="70" />
-          <WeatherCard city="Kitchener" current="9" high="13" low="1" humidity="80" />
-          <WeatherCard city="Oakville" current="10" high="15" low="0" humidity="90" />
+          {props.weatherData.map(weatherDatum => <WeatherCard {...weatherDatum}/>)}
         </div> 
     );
 }
--- src/WeatherDashboard.js
+++ src/WeatherDashboard.js
@@ -2,11 +2,25 @@ import CityWeather from './CityWeather';
 import AddCityForm from './AddCityForm';
 import './WeatherDashboard.css';
 
+const testData = [
+  {
+    city:"Toronto",current:"8",high:"11",low:"2",humidity:"70"
+  },
+  {
+    city:"Kitchener",current:"9",high:"13",low:"1",humidity:"80"
+  },
+  {
+    city:"Oakville",current:"10",high:"15",low:"0",humidity:"90"
+  }
+];
+
+
 function WeatherDashboard(){
-    return (       
+
+  return (       
      <div className="WeatherDashboard">
         <AddCityForm />
-        <CityWeather />
+        <CityWeather weatherData = {testData} />
       </div>
     );
 }
